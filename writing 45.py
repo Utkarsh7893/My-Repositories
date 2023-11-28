30ecc29e7eae3f53e929d8632376929e08558dfc
Utkarsh Raj <149226691+Utkarsh7893@users.noreply.github.com>
2023-11-28 23:08:13 +0530

    Typing Practice using conditional statements (CLI)
    I made a Typing Practice CLI using conditional statements in python. Its under construction. Kindly provide feedback if you like it.

---
+++ writing 45.py
@@ -0,0 +1,186 @@
+a=("Hyy there, My name is Utkarsh Raj and I am the inventor of this App...")
+b=("In this Application you will be provided by a interface where you can practice and enhance your writing skills..")
+d=("Can i know what is your age?")
+print(a)
+print(b)
+print(d)
+f=int(input(":"))
+if(f>=18) and (f>0):
+    print("Welcome To Typing Practice by UTK:")
+    print("Thank You For Chooing this App, Any inconvinence is Regreted!:")
+elif(f<=18) and (f>0):
+    print("Welcome To Typing Practice by UTK: As you are a junior you can not get Access to the Advnace Package!:")
+    print("Thank You For choosing this App, Any inconvinence is regreted!:")
+else:
+    print("!!Invalid Age!!")
+    quit()
+h=str(input("Press p to proceed Further_"))
+if(h=="p" or h=="P"):
+    print("Stage 01: Beginner (P01)")
+    print("Stage 02: Intermediate (P02)")
+    print("Stage 03: Advance (P03)")
+else:
+    v=str(input("Press Q to Quit_"))
+    o=str(input("Press B to Go Back_"))
+    if (v=="q") or (v=="Q"):
+        quit()    
+    if(o=="b") or (o=="B"):
+        print("Stage 01: Beginer")
+        print("Stage 02: Intermediate")
+        print("Stage 03: Advance")
+    else:
+        print("No Fields Defines")
+op=input("Press Coressponding Variables to process the level_")
+if(op=="P01"):
+    print("Welcome Beginner")
+        #### Data Part
+    print("Level 01:")
+    p902="------>"
+    p90="Python is an interpreted object-oriented high-level programming language with dynamic semantics" 
+    i90p3="------>"  
+    i90p="Its high-level built in data structures combined with dynamic typing and dynamic binding make" 
+    k90p123="------>"
+    k90p12="as well as for use as a scripting or glue language to connect existing components together" 
+    o90p123="------>"
+    o90p13="and therefore reduces the cost Python supports modules and packages which encourages"
+    p90p145="------>"
+    p90p14="and the extensive standard library are available in source or binary form without"
+    p90o6=">>>>>>>>"
+    p90o="Dear User, You are requested to Write the provided text in the given block:__"
+    print (p902+p90)
+    f=7.902090*280571038
+    print(p90o6+p90o)
+    h=4444//7210**7
+    print("Note: Do not print enter to change the line...")
+    k=9820**172
+    count=0
+    countpx=0
+    p890=input("Type Here_ ")#line 01
+    if(p890==p90):
+        print("Line 01 is Correct")
+        count=count+1
+    else:
+        print("Line 01 is incorrect")
+    print(i90p3+i90p)
+    p890123=input("Type Here_")#line 02
+    if(p890123==i90p):
+        print("Line 02 is correct")
+        count=count+1
+    else:
+        print("Line 02 is incorrect")
+    print(k90p123+k90p12)
+    p673=input("Type Here_")#line 03
+    if(p673==k90p12):
+        print("Line 03 is correct")
+        count=count+1
+    else:
+        print("Line 03 is incorrect")
+    print(o90p123+o90p13)
+    p8980=input("Type Here_")#line 04
+    if(p8980==o90p13):
+        print("Line 04 is correct")
+        count=count+1
+    else:
+        print("Line 04 is incorrect")
+    print(p90p145+p90p14)
+    pfghja=input("Type Here_")#line 05
+    if(pfghja==p90p14):
+        print("Line 05 is correct")
+        count=count+1     
+    else:
+        print("Line 05 is incorrect")
+    if (count>=3):
+        print("You are qualified for LEVEL:2")
+        print("No Of Questions correct:",count,"/5")
+        countpx=countpx+10
+        if countpx==10:
+            print("Press p to proceed to the next level: Level 1 --------> Level 2...")
+            gh=input("_ ")
+            if (gh=="p") or (gh=="P"):
+                print("Welcome User: ",end="\n")
+                print("Qualified for Level 2....",end="\n")
+            else:
+                print("Invalid")
+                 #### Data Part
+            gh=("@#@#")
+            sd=("Dear User you have to type the given data in the block given below..") 
+            gh01=("----->")
+            sd01= ("Linux: Python comes preinstalled with popular Linux distros such as Ubuntu and Fedora.To check which version of Python you re running type python in the terminal emulator.")
+            gh02=("----->")
+            sd02=("To print something on the console, print() function is used. This function also adds a newline after our message is printed(unlike in C)")
+            gh03=("----->")
+            sd03=("Emphasis on code readability, shorter codes, ease of writing Programmers can express logical concepts in fewer lines of code in comparison to languages such as C++ or Java.")
+            gh04=("----->")
+            sd04=("There are no separate compilation and execution steps like C and C++. Directly run the program from the source code.")
+            gh05=("----->")
+            sd05=("In Python, no need to take care about low-level details such as managing the memory used by the program.")
+            countut=0
+            countts=0
+            f89098=(6**9717+8971)
+            print(gh+sd)
+            klasjid=8**2+8273/23
+            print(gh01+sd01)
+            sd002=input("Type_Here_ ") #Line 01
+            if (sd002==sd01):
+                print("Line 01 is correct")
+                countut=countut+1
+            else:
+                print("Line 01 is incorrect")
+                print(gh02+sd02)
+            sd003=input("Type_Here_ ") #Line 02
+            if (sd003==sd02): 
+                print("Line 02 is correct")
+                counut=countut+1
+            else:
+                print("Line 02 is incorrect")
+                print(gh03+sd03)
+            sd004=input("Type_Here_") #Line 03
+            if (sd004==sd03):
+                print("Line 03 is correct")
+                countut=countut+1
+            else:
+                print("Line 03 is incorrect")
+                print(gh04+sd04)
+            sd005=input("Type_Here_ ") #Line 04
+            if (sd005==sd04):
+                print("Line 04 is correct")
+                countut=countut+1
+            else:
+                print("Line 04 is incorrect")
+                print(gh05+sd05)
+            sd006=input("Type_Here_ ") #Line 05
+            if(sd006==sd05):
+                print("Line 05 is correct")
+                countut=countut+1
+            else:
+                print("Line 05 is incorrect")
+                jashfuilhae=6**981+8019
+            if (countut>=3):
+                print("No. of Correct Questions: ",countut,"/5")
+                print ("\033[0m""!!!!!!Congratulations User!!!!!!!""\033[0m")
+                print("Succefully Completed the Level 02")
+                countts=countts+10  
+                if (countts==10):
+                    print("Press p to proceed further__ ")
+                    gh898=input("_")
+                    if gh898=="p" or gh898=="P":
+                        print("Welcome To Level 03: ")
+
+                    else:
+                        print("Not Qualified for Level 03")
+                else:
+                    print("Failed in Qualifying Level-02")                
+    else:
+        print("Not Quallified for Level 2")
+elif(op=="P02"):
+    print("Welcome Intermediater")
+    print("Level:01")
+elif(op=="P03"):
+    print("Welcome Advance Player")
+    print("Level:01")
+else:
+    print("Not Valid")
+    quit()
+
+
+
